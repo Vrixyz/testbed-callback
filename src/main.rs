344b955b130e7b60fab18f1005459095b6289e28
BERGER Thierry <thierry.berger@externe.e-i.com>
2018-08-13 13:47:07 +0200

    example with specs rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,89 @@
+#![feature(pattern_parentheses)]
+
 extern crate nphysics_testbed2d;
 extern crate nalgebra as na;
 extern crate ncollide2d;
 extern crate nphysics2d;
+extern crate specs;
 
-use nphysics2d::world::World;
 use nphysics_testbed2d::Testbed;
+use specs::prelude::*;
 
-pub struct Data {
-  pub data: String,
+//struct PhysicsWorld(std::cell::RefCell<nphysics2d::world::World<f32>>);
+struct PhysicsWorld(nphysics2d::world::World<f32>);
+
+impl Default for PhysicsWorld {
+    fn default() -> PhysicsWorld {
+        //PhysicsWorld(std::cell::RefCell::new(nphysics2d::world::World::new()))
+        PhysicsWorld(nphysics2d::world::World::new())
+    }
+}
+
+struct DummySystem {
+  counter: u32,
+}
+
+impl DummySystem {
+    fn new() -> DummySystem {
+        DummySystem { counter: 0 }
+    }
+}
+impl<'a> System<'a> for DummySystem
+     {
+    type SystemData = (
+        Write<'a, PhysicsWorld>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+      if self.counter < 10 {
+        let (mut world) = data;
+
+        let mut world = &mut (world.0).0;
+        use na::{Isometry2, Point2, Vector2};
+        use ncollide2d::shape::{Cuboid, ShapeHandle};
+        use nphysics2d::object::{BodyHandle, Material};
+        use nphysics2d::volumetric::Volumetric;
+
+        // Adapted from http://nphysics.org/rigid_body_simulations_with_contacts/ and some demo examples.
+        // If something shorter exists, please share
+        let cuboid = ShapeHandle::new(Cuboid::new(Vector2::new(1.0, 2.0)));
+        let local_inertia = cuboid.inertia(1.0);
+        let local_center_of_mass = cuboid.center_of_mass();
+        let rigid_body_handle = world.add_rigid_body(
+            Isometry2::new(Vector2::x() * 2.0, na::zero()),
+            local_inertia,
+            local_center_of_mass,
+        );
+        self.counter = self.counter + 1;
+      }
+      println!("dummy System");
+    }
 }
 
 fn main() {
-  let data = Data { data: "test" };
-  let testbed = Testbed::new(World::new_empty());
+
+  // nphysics initialization
+  let physics_world = nphysics2d::world::World::new();
+
+
+  // Specs initialization
+  let ecs_world = std::cell::RefCell::new(specs::World::new());
+  ecs_world.borrow_mut().add_resource(PhysicsWorld(physics_world));
+  let dispatcher = DispatcherBuilder::new()
+        .with(DummySystem::new(), "dummy_system", &[])
+        .build();
+
+  let mut dispatcher = std::cell::RefCell::new(dispatcher);
+  
+  // Testbed initialization
+  let mut testbed = Testbed::new(
+    // FIXME: This cannot compile because it's moved into PhysicsWorld resource.
+    physics_world
+  );
   testbed.add_callback( move |_,_,_| {
-    println!("non static data from outside the callback block: {}", data.data);
-  }
+    let mut ecs_world = ecs_world.borrow_mut();
+    dispatcher.borrow_mut().dispatch(&mut ecs_world.res);
+    ecs_world.maintain();
+  });
   testbed.run();
 }
