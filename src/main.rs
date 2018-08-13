1c203e0f82d7de7ad11c4a7de167e7b3c431bb05
Thierry Berger <contact@thierryberger.com>
2018-08-13 08:25:09 +0200

    Add dependencies to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate nphysics_testbed2d;
+extern crate nalgebra as na;
+extern crate ncollide2d;
+extern crate nphysics2d;
+
+use nphysics2d::world::World;
+use nphysics_testbed2d::Testbed;
+
 pub struct Data {
   pub data: String,
 }
@@ -6,7 +14,7 @@ fn main() {
   let data = Data { data: "test" };
   let testbed = Testbed::new(World::new_empty());
   testbed.add_callback( move |_,_,_| {
-    println!("{}", data.data);
+    println!("non static data from outside the callback block: {}", data.data);
   }
   testbed.run();
 }
